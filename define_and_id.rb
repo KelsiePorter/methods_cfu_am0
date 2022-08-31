9030b2d91b395f171e7c5ee005163cfd18725726
Kelsie Porter <krporter22@gmail.com>
2022-08-30 18:57:05 -0600

    Add answers for define_and_id.rb CFU

--- define_and_id.rb
+++ define_and_id.rb
@@ -1,5 +1,6 @@
 # Run each line of code below (either from this file or in IRB individually).
-#  Then, in a ruby comment, write 1-2 sentences describing what is happening, using ALL the involved vocabulary terms you've learned in this lesson so far.
+#  Then, in a ruby comment, write 1-2 sentences describing what is happening, using ALL the involved vocabulary terms
+# you've learned in this lesson so far.
 
 # EXAMPLE
 # The downcase method is called on the string object "Hello World"
@@ -9,19 +10,24 @@
 
 # YOU DO the rest:
 
-
+# The include? method uses an argument to call on the string object "Hello World" to determine if the string includes "Hello"
+#  The return value is true
 "Hello World".include?("Hello")
 
-
+# The end_with? method uses an argument to call on the string obeject "Hello World" to determine if the last value of the string includes "Hello"
+# The return value is false
 "Hello World".end_with?("Hello")
 
-
+# The end_with? method uses an argument to call on the string obeject "Hello World" to determine if the last value of the string includes "rld"
+# The return value is true
 "Hello World".end_with?("rld")
 
-
+# The even? method is called on the integer 12 to determine if the integer is even
+# No arguments are passed
+# The return value is true
 12.even?
 
-
+# The next menthod is called on the integer 18 to determine what the immediate next (higher) integer will be in numerical order.
+# No arguments are passed
+# The return value is 19
 18.next
-
-
